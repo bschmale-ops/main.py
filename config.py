77063b3d60e65fb68ad62f186e89561cc72ca7b8
shiseii <b_schmale@gmx.de>
2025-10-17 22:19:20 +0200

    config.py

--- config.py
+++ config.py
@@ -7,9 +7,17 @@ DISCORD_TOKEN = "----"
 # Twitch API
 TWITCH_CLIENT_ID= "rwaq6wdx7z8gqt7gywf1xll8p0il22"
 TWITCH_CLIENT_SECRET= "rwaq6wdx7z8gqt7gywf1xll8p0il22"
+TWITCH_USERNAME = "shiseii"
 
 # Grid.gg API
 GRID_API_KEY = "jmoLQscQ10EDnFgzBCosixLkTE31tVXStEoLESXw"
 
 # Channel IDs
 ANNOUNCEMENT_CHANNEL_ID = 1162297673920024667
+
+# Auto-Subscribe Teams
+AUTO_SUBSCRIBE_TEAMS = [
+    'Falcons', 'MOUZ', 'Team Spirit', 'Team Vitality', 'The Mongolz',
+    'FURIA', 'Natus Vincere', 'FaZe', '3DMAX', 'Astralis', 
+    'G2', 'Aurora', 'Liquid', 'M80'
+]
